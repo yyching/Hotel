668e0b6288a2757405fd63f9cfd79617e28af80e
raynold04 <164487002+raynold04@users.noreply.github.com>
2024-12-22 03:48:59 +0800

    update

--- Hotel/Models/ViewModels.cs
+++ Hotel/Models/ViewModels.cs
@@ -138,11 +138,11 @@ public class RoomDetailsVM
 {
     public string CategoryID { get; set; }
 
-    [Display(Name = "Check In Date")]
+    [Display(Name = "Check In Date :")]
     [DataType(DataType.Date)]
     public DateOnly CheckInDate { get; set; }
 
-    [Display(Name = "Check Out Date")]
+    [Display(Name = "Check Out Date :")]
     [DataType(DataType.Date)]
     public DateOnly CheckOutDate { get; set; }
 
